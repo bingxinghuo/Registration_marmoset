b604c888071e0f5a5f3c3626bfaf5f2f3bd6fb8d
Bingxing Huo <bingxing.huo@gmail.com>
2020-04-28 16:45:00 -0400

    updated marmoset summary volume generation to keep density values as float

--- result_to_registered_flip.py
+++ result_to_registered_flip.py
@@ -9,23 +9,44 @@ Created on Mon Dec 30 18:05:00 2019
 import SimpleITK as sitk
 import numpy as np
 import sys
+import os
 
 resultfile = sys.argv[1]
 atlasfile = sys.argv[2]
 flipfile=sys.argv[3]
 
-reader = sitk.ImageFileReader()
-reader.SetFileName(resultfile)
-img = reader.Execute()
-arr = sitk.GetArrayFromImage(img)
-arr2 = np.swapaxes(arr,1,2)
-arr2=np.float32(arr2)
-img1 = sitk.GetImageFromArray(arr2)
+if resultfile.endswith('.tif'):
+    reader = sitk.ImageFileReader()
+    reader.SetFileName(resultfile)
+    img = reader.Execute()
+    arr0 = sitk.GetArrayFromImage(img)
+    arr = np.swapaxes(arr0,1,2)
+    arr=np.float32(arr)
+elif resultfile.endswith('.mat'):
+    import h5py
+    f=h5py.File(resultfile,'r') # read file -v7.3 mat
+    a=f['neurondensityvol'] 
+    C=a.len() # number of cells
+    a1=a[0][0] # access individual cell
+    a1=f.get(a1).value # get numpy array
+    matsize=a1.shape
+    matsizel=list(matsize)
+    matsizel.append(3)
+    matsize=tuple(matsizel)
+    arr=np.empty(matsize)
+    arr[:,:,:,0]=a1
+    for c in range(1,C):
+        a1=a[c][0] # access individual cell
+        a1=f.get(a1).value # get numpy array
+        arr[:,:,:,c]=a1
+            
 
 atlasimg=sitk.ReadImage(atlasfile)
-
-img1.SetSpacing(atlasimg.GetSpacing())
-img1.SetOrigin(atlasimg.GetOrigin())
-img1.SetDirection(atlasimg.GetDirection())
-
-sitk.WriteImage(img1,flipfile)
+filename, file_extension = os.path.splitext(flipfile)
+C=arr.shape[3]        
+for c in range(C):
+    img1 = sitk.GetImageFromArray(arr[:,:,:,c],False)
+    img1.SetSpacing(atlasimg.GetSpacing())
+    img1.SetOrigin(atlasimg.GetOrigin())
+    img1.SetDirection(atlasimg.GetDirection())       
+    sitk.WriteImage(img1,filename+'_'+str(c+1)+file_extension)
